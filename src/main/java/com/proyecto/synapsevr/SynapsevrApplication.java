79b6855f224b1b7e4dd503413d5394c42de3f2b3
Gonzalo <gska2010@gmail.com>
2025-09-09 18:01:35 -0500

    Initial commit: creacion del proyecto

---
+++ src/main/java/com/proyecto/synapsevr/SynapsevrApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.synapsevr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynapsevrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynapsevrApplication.class, args);
+	}
+
+}
